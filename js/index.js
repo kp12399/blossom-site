a196930cf7416383d8dd1af19b23ff4b9068aec7
kp12399 <kp12399@gmail.com>
2019-12-11 14:32:50 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,268 @@
+
+
+
+
+var agePrompt=prompt("What is your age?");
+ 
+if (agePrompt>=18) alert('Welcome!')
+else {
+alert('Sorry, you are too Young')
+
+}
+
+
+const strains = [
+
+
+
+{
+
+name: `Girl Scout Cookies`,
+rate: `4.6`,
+description: `This classic strain is a sativa dominant hybrid, a cross between Durban Poison and OG Kush. Recommended for patients suffering from severe pain, nausea, or appetite loss.`,
+price: `13.99`,
+image: `GSC.jpeg`,
+},
+
+{
+
+    name: `Love Potion`,
+    rate: `4.4`,
+    description: `Love Potion is a heavy sativa strain that is great for patients with depression, pain, and fatigue, due to its euphoric uplifting effects that provide feelings of happiness.`,
+    price: `12.99`,
+    image: `lovepotion.jpeg`,
+    },
+
+    {
+
+        name: `Blue Cookies`,
+        rate: `4.6`,
+        description: `This classic strain is a sativa dominant hybrid, a cross between Durban Poison and OG Kush. Recommended for patients suffering from severe pain, nausea, or appetite loss.`,
+        price: `11.00`,
+        image: `bluecookies.jpeg`,
+        },
+
+        {
+
+            name: `Black Dream`,
+            rate: `3.5`,
+            description: `This classic strain is a sativa dominant hybrid, a cross between Durban Poison and OG Kush. Recommended for patients suffering from severe pain, nausea, or appetite loss.`,
+            price: `8.99`,
+            image: `blackdream.jpeg`,
+            },
+
+            {
+
+                name: `Bruce Banner`,
+                rate: `3.1`,
+                description: `This classic strain is a sativa dominant hybrid, a cross between Durban Poison and OG Kush. Recommended for patients suffering from severe pain, nausea, or appetite loss.`,
+                price: `10.00`,
+                image: `brucebanner.jpeg`,
+                },
+
+                {
+
+                    name: `Tropaya`,
+                    rate: `4.5`,
+                    description: `This classic strain is a sativa dominant hybrid, a cross between Durban Poison and OG Kush. Recommended for patients suffering from severe pain, nausea, or appetite loss.`,
+                    price: `8.49`,
+                    image: `tropaya.jpeg`,
+                    },
+
+                    {
+
+                        name: `BC Rockstar`,
+                        rate: `4.5`,
+                        description: `This classic strain is a sativa dominant hybrid, a cross between Durban Poison and OG Kush. Recommended for patients suffering from severe pain, nausea, or appetite loss.`,
+                        price: `11.49`,
+                        image: `bcrockstar.png`,
+                        },
+
+                        {
+
+                            name: `Pineapple Express`,
+                            rate: `5`,
+                            description: `This classic strain is a sativa dominant hybrid, a cross between Durban Poison and OG Kush. Recommended for patients suffering from severe pain, nausea, or appetite loss.`,
+                            price: `14.49`,
+                            image: `pineexpress.png`,
+                            },
+
+                            {
+
+                                name: `Mimosa`,
+                                rate: `3.5`,
+                                description: `This classic strain is a sativa dominant hybrid, a cross between Durban Poison and OG Kush. Recommended for patients suffering from severe pain, nausea, or appetite loss.`,
+                                price: `9.49`,
+                                image: `mimosa.png`,
+                                },
+
+                                {
+
+                                    name: `Raspberry Cough`,
+                                    rate: `3.9`,
+                                    description: `This classic strain is a sativa dominant hybrid, a cross between Durban Poison and OG Kush. Recommended for patients suffering from severe pain, nausea, or appetite loss.`,
+                                    price: `11.00`,
+                                    image: `raspberry.png`,
+                                    },
+
+                                    {
+
+                                      name: `Space Queen`,
+                                      rate: `4.1`,
+                                      description: `Space Queen is a legendary hybrid created by famed breeder Vic High of BC Growers Association. A cross between Romulan and Cinderella 99, Space Queen presents a wide array of phenotypes.`,
+                                      price: `11.49`,
+                                      image: `spacequeen.png`,
+                                      },
+
+                                    
+                                      {
+
+                                        name: `Lindsay OG`,
+                                        rate: `3.9`,
+                                        description: `Lindsay OG strain is an award-winning indica dominant flower. It is most known for its potent sedative effects and wonderful pungent aroma. Great for pain relief.`,
+                                        price: `13.00`,
+                                        image: `lindsay.png`,
+                                        },
+  
+                                    
+                                        {
+
+                                          name: `Strawberry Banana`,
+                                          rate: `4.3`,
+                                          description: `Strawberry Banana is an indica developed by DNA Genetics in collaboration with Serious Seeds. A genetic cross of Crockett’s Banana Kush and the “Strawberry” phenotype of Bubble Gum.`,
+                                          price: `8.49`,
+                                          image: `strawberry.png`,
+                                          },
+    
+                                    
+                                          {
+
+                                            name: `Gorilla Glue #4`,
+                                            rate: `5`,
+                                            description: `Gorilla Glue by GG Strains is a potent hybrid strain that delivers heavy euphoria and relaxation, leaving you feeling “glued” to the couch and free of any pain or worries.`,
+                                            price: `16.99`,
+                                            image: `gorillaglue.png`,
+                                            },
+      
+                                    
+                                            {
+
+                                              name: `Peyote Cookies`,
+                                              rate: `4.3`,
+                                              description: `This classic strain is a sativa dominant hybrid, a cross between Durban Poison and OG Kush. Recommended for patients suffering from severe pain, nausea, or appetite loss.`,
+                                              price: `9.49`,
+                                              image: `peyotecookies.png`,
+                                              },
+        
+                                    
+                                              {
+
+                                                name: `Vader OG`,
+                                                rate: `3.7`,
+                                                description: `Vader OG by Ocean Grown Seeds is the namesake strain of one of OGS’s master growers, Vader. This cross began in 2006 with the combination of SFV OG and Larry OG.`,
+                                                price: `10.00`,
+                                                image: `vaderog.png`,
+                                                },
+          
+                                    
+                                                {
+
+                                                  name: `Greasy Pink`,
+                                                  rate: `4.2`,
+                                                  description: `Greasy Pink Kush, as coveted as its OG Kush relative, is an indica-dominant hybrid with powerful body-focused effects, in its exceptional variations.`,
+                                                  price: `14.49`,
+                                                  image: `greasypink.png`,
+                                                  },
+            
+                                    
+                                                  {
+
+                                                    name: `Gushers`,
+                                                    rate: `3.9`,
+                                                    description: `Gushers by Compass Genetics is an alluring mixture of fruity Floridian strains mixed against a currently unknown cross. `,
+                                                    price: `11.99`,
+                                                    image: `gushers.png`,
+                                                    },
+              
+                                    
+                                                    {
+
+                                                      name: `Orange Sherbet`,
+                                                      rate: `4.6`,
+                                                      description: `Orange Sherbet is a slightly indica dominant hybrid strain that’s a descendant of the super tasty Sunset Sherbet strain and perhaps the line of Orange strains.`,
+                                                      price: `12.49`,
+                                                      image: `orange.png`,
+                                                      },
+                
+                                    
+                                                      {
+
+                                                        name: `Premium Shake`,
+                                                        rate: `3.6`,
+                                                        description: `This Ounce of shake and trim comes only from the leftovers of our Premium AAA and AAAAstrains.  Including but not limited to a combination mix of God’s Green Crack, GSC, and Pineapple`,
+                                                        price: `6.49`,
+                                                        image: `shake.png`,
+                                                        },
+                  
+                                                                                                                              
+                                    
+
+
+
+]
+
+function addHtmlData (product){
+    return `<li class = "item">
+    <article id="weedstrains">
+    <div><img class = "photo" src="img/${product.image}" alt="Product Image"></div>
+    <h3>${product.name}</h3>
+    <dd class="center"><span>${product.rate} <i class="material-icons">star</i></span></dd>
+    <p>${product.description}</p>
+    <dl>
+
+      <dt>Weight Options (Grams)</dt>
+      <dd><span>1</span>  •  <span>3.5</span>  •  <span>7</span>  •  <span>14</span>  •  <span>28</span></dd>
+
+
+
+    </dl>
+    <div>
+      <span class = "price"><ins>$${product.price}/g</ins></span>
+
+    </div>
+    <div class = "btn"><a href="product.html">See More</a></div>
+  </article>
+  </li>`;
+}
+
+function showstrains(product){
+const arrayofStrains = product.map(addHtmlData);
+const String = arrayofStrains.join('\n');
+document.getElementById('weedstrains').innerHTML = String;
+
+}
+
+showstrains(strains);
+
+
+
+function isMatchingName(prod){
+  if(prod.name.toUpperCase().includes(this.trim().toUpperCase())){
+    return true;
+  } 
+  else{
+    return false;
+  }
+
+  
+  
+  
+}
+
+function searchStrainName(){
+  const nameImSearchingFor = event.target.value;
+  showstrains(strains.filter(isMatchingName, nameImSearchingFor));
+}
+  document.getElementById('strainname').addEventListener('input', searchStrainName); 
+
+  
